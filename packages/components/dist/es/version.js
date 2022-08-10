4abae30eb180b00d8fead2aa82331d24fa4dadb8
husen <husen@zdns.cn>
2022-08-10 09:08:31 +0800

    changeset changelog 测试

--- packages/components/dist/es/version.js
+++ packages/components/dist/es/version.js
@@ -1,9 +1,9 @@
 export var version = {
   "@dne-design/pro-bar": "2.0.1",
   "@dne-design/pro-button": "2.0.1",
-  "@dne-design/pro-components": "2.0.1",
+  "@dne-design/pro-components": "2.0.3",
   "@dne-design/pro-foo": "2.0.1",
-  "@dne-design/pro-header": "0.0.1",
+  "@dne-design/pro-header": "0.0.3",
   "@dne-design/pro-layout": "2.0.1",
-  "@dne-design/pro-utils": "2.0.1"
+  "@dne-design/pro-utils": "2.0.2"
 };
--- packages/components/dist/lib/version.js
+++ packages/components/dist/lib/version.js
@@ -25,11 +25,11 @@ module.exports = __toCommonJS(version_exports);
 var version = {
   "@dne-design/pro-bar": "2.0.1",
   "@dne-design/pro-button": "2.0.1",
-  "@dne-design/pro-components": "2.0.1",
+  "@dne-design/pro-components": "2.0.3",
   "@dne-design/pro-foo": "2.0.1",
-  "@dne-design/pro-header": "0.0.1",
+  "@dne-design/pro-header": "0.0.3",
   "@dne-design/pro-layout": "2.0.1",
-  "@dne-design/pro-utils": "2.0.1"
+  "@dne-design/pro-utils": "2.0.2"
 };
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
